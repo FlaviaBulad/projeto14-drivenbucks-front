28f62062d8ae4e8fada3e42b09d00b6c0ae6280f
Flávia Bulad <flavia.bulad@gmail.com>
2022-07-06 18:44:35 -0300

    feat: logo component

---
+++ src/components/layout/Logo.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export default function Logo() {
+  return <Title>Drivenbucks</Title>;
+}
+
+const Title = styled.div`
+  font: 700 40px "Quicksand", sans-serif;
+  color: #d57e52;
+`;
