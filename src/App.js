d375e3e829c2c666b7c986aff18fa58eaecbbeff
Flávia Bulad <flavia.bulad@gmail.com>
2022-07-06 14:06:57 -0300

    feature: add routes to app.js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import WelcomePage from "./components/pages/Welcome/WelcomePage";
+import SignInPage from "./components/pages/SignIn/SignInPage";
+import SignUpPage from "./components/pages/SignUp/SignUpPage";
+import MainPage from "./components/pages/Main/MainPage";
+import BasketPage from "./components/pages/Basket/BasketPage";
+import CheckoutPage from "./components/pages/Checkout/CheckoutPage";
+
 import "./assets/css/reset.css";
 import "./assets/css/index.css";
 
 function App() {
-  return <h1>Drivenbucks front</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/sign-in/" element={<SignInPage />} />
+        <Route path="/sign-up/" element={<SignUpPage />} />
+        <Route path="/main/" element={<MainPage />} />
+        <Route path="/basket/" element={<BasketPage />} />
+        <Route path="/checkout/" element={<CheckoutPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
