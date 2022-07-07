cc918ab912c10fe784c661d8db21e79b06a15efe
Flávia Bulad <flavia.bulad@gmail.com>
2022-07-06 21:40:07 -0300

    feat: building sign-up page, missing style

--- src/components/pages/SignUp/SignUpPage.js
+++ src/components/pages/SignUp/SignUpPage.js
@@ -1,3 +1,121 @@
+import styled from "styled-components";
+import axios from "axios";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import Logo from "../../layout/Logo.js";
+import Spinner from "../../libs/Spinner.js";
+
 export default function SignUpPage() {
-  return <h1>Página de cadastro</h1>;
+  const signUpDataObject = {
+    name: "",
+    email: "",
+    password: "",
+    confirm_password: "",
+  };
+
+  const navigate = useNavigate();
+
+  const [signUpData, setSignUpData] = useState(signUpDataObject);
+  const [isLoading, setIsLoading] = useState(false); //loader spinner state
+
+  function OnChange(e) {
+    setSignUpData({ ...signUpData, [e.target.name]: e.target.value });
+  }
+
+  async function SignUpDataToAPI(e) {
+    //sending all the data to api and redirectioning user to sign-in page if successfull
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      await axios.post("https://drivenbucks.herokuapp.com/sign-up", {
+        ...signUpData,
+      });
+
+      setIsLoading(false);
+      alert("Cadastro criado com sucesso!");
+      navigate("/");
+    } catch (error) {
+      setIsLoading(false);
+      alert(`Erro ao cadastrar: ${error}`);
+    }
+  }
+
+  function BuildingSignUpForms() {
+    return (
+      <>
+        <Logo />
+        <h2>
+          Olá!
+          <p>Preencha os dados para continuar</p>
+        </h2>
+        <form onSubmit={SignUpDataToAPI}>
+          <input
+            type="text"
+            placeholder="Seu nome"
+            name="name"
+            onChange={OnChange}
+            value={signUpData.name}
+            required
+          />
+          <input
+            type="email"
+            placeholder="Seu melhor email"
+            name="email"
+            onChange={OnChange}
+            value={signUpData.email}
+            required
+          />
+
+          <input
+            type="password"
+            placeholder="senha"
+            name="password"
+            onChange={OnChange}
+            value={signUpData.password}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Confirme a senha"
+            name="password_confirmation"
+            onChange={OnChange}
+            value={signUpData.password_confirmation}
+            required
+          />
+
+          <button type="submit" disabled={isLoading}>
+            {isLoading ? (
+              <Spinner
+                type="ThreeDots"
+                color="#FFFFFF"
+                height={50}
+                padding={50}
+                width={50}
+              />
+            ) : (
+              "Cadastrar"
+            )}
+          </button>
+        </form>
+
+        <Link to="/sign-in">
+          Já possui uma conta? <span>Entrar</span>
+        </Link>
+      </>
+    );
+  }
+
+  const signUpForms = BuildingSignUpForms();
+
+  return <Container>{signUpForms}</Container>; //renders everything on the page
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #efefef;
+`;
--- src/components/pages/Welcome/WelcomePage.js
+++ src/components/pages/Welcome/WelcomePage.js
@@ -1,62 +1,67 @@
-import styled from "styled-components"
+import styled from "styled-components";
 import { Link } from "react-router-dom";
-import welcome from "../../../assets/images/layout/welcome.png"
+import welcome from "../../../assets/images/layout/welcome.png";
 export default function WelcomePage() {
   return (
     <Container>
       <h1>Drivenbucks</h1>
-      <img src={welcome} alt=""/>
+      <img src={welcome} alt="" />
       <div>
         <h2>Bem-vindo!</h2>
-        <p>Encontre as maiores delícias das cafeterias e receba no conforto da sua casa!</p>
-        <Link to="/sign-in/"><button>Começar</button></Link>
+        <p>
+          Encontre as maiores delícias das cafeterias e receba no conforto da
+          sua casa!
+        </p>
+        <Link to="/sign-in/">
+          <button>Começar</button>
+        </Link>
       </div>
     </Container>
   );
-};
+}
 
 const Container = styled.div`
-  background-color:#EFEFEF;
+  background-color: #efefef;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  h1{
+  h1 {
     margin-top: 40px;
-    font: 700 40px 'Quicksand', sans-serif;
+    font: 700 40px "Quicksand", sans-serif;
     color: #d57e52;
-  };
-  img{
+  }
+  img {
     margin-top: 20px;
     max-width: 300px;
-  };
-  div{
-    background-color:#FFFFFF;
-    border-radius:20px;
+  }
+  div {
+    background-color: #ffffff;
+    border-radius: 20px;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    padding:25px;
-    gap:20px;
-  };
-  h2{
-    font: 700 30px 'Quicksand', sans-serif;
+    padding: 25px;
+    gap: 20px;
+  }
+  h2 {
+    font: 700 30px "Quicksand", sans-serif;
     color: #654c41;
-  };
-  p{
-    font: 400 14px 'Quicksand', sans-serif;
+  }
+  p {
+    font: 400 14px "Quicksand", sans-serif;
     color: #545454;
     text-align: center;
-  };
-  button{
+  }
+  button {
     width: 150px;
     height: 40px;
     border-radius: 50px;
-    background-color:#654c41;
+    background-color: #654c41;
     border: none;
-    color: #FFFFFF;
-    font: 700 14px 'Quicksand', sans-serif;
+    color: #ffffff;
+    font: 700 14px "Quicksand", sans-serif;
   }
-`
+`;
 // #EFEFEF
