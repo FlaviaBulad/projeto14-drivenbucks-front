e91249ab92935eb9e0cc07ce25d1d5bb0a33769f
PauloGabrieel <gabrieeel.c@gmail.com>
2022-07-08 17:54:58 -0300

    feat: Criado compodente que vai renderizar os produtos que vierem da API

---
+++ src/components/layout/CardProduct.js
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+
+import apple from "../../assets/images/products/apple-pie.png";
+export default function CardProduct(){
+    return (
+        <Card>
+        <img src={apple} />
+        <div>
+          <div>
+            <h2>Torta de maçã</h2>
+            <p>Deliciosa, perfeita para compartilhar com a familia!</p>
+            <span>* * * * *</span>
+          </div>
+          
+          <ValueContainer>
+            <span>R$21,50</span>
+            <button>Add to cart</button>
+          </ValueContainer>
+            
+        </div>
+      </Card>
+    );
+};
+const Card = styled.div`
+    width: 100%;
+    height: 150px;
+    /* background-color: #EFEFEF; */
+    display: flex;
+    gap: 10px;
+    img{
+      height: 150px;
+      width:150px;
+      border-radius: 5px;
+    };
+    
+    h2{
+      margin-bottom: 10px;
+      font: 700 16px 'Quicksand', sans-serif;
+        
+    };
+    p{
+      margin-bottom: 10px;
+      font: 700 10px 'Quicksand', sans-serif;
+    };
+`
+ const ValueContainer = styled.div`
+  margin-top: 20px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  span{
+    font: 700 22px 'Quicksand', sans-serif;
+  };
+  button{
+    background-color:  #654c41;
+    height: 35px;
+    width: 100px;
+    color: #FFFFFF;
+    border: none;
+    border-radius: 5px;
+    font: 700 12px 'Quicksand', sans-serif; 
+    cursor: pointer;
+  }
+ `
--- src/components/pages/Main/MainPage.js
+++ src/components/pages/Main/MainPage.js
@@ -1,3 +1,77 @@
+import styled from "styled-components";
+
+import { FiMenu,FiLogOut } from "react-icons/fi";
+
+import Logo from "../../layout/Logo";
+import banner from "../../../assets/images/layout/banner.png"
+import CardProduct from "../../layout/CardProduct.js"
 export default function MainPage() {
-  return <h1>Página principal com os produtos</h1>;
+  return (
+    <>
+    <ContainerHeader>
+        <BackIcon>
+          <FiMenu color={"#FFFFFF"} size={30}></FiMenu>
+        </BackIcon>
+        <Logo></Logo>
+        <BackIcon>
+          <FiLogOut color={"#654C41"} size={30}></FiLogOut>
+        </BackIcon>
+      </ContainerHeader>
+    <Container>
+      <Banner src={banner} alt="banner"/>
+      <ContainerProducts>
+        <CardProduct></CardProduct>
+      </ContainerProducts>
+      
+    </Container>
+    
+    </>
+    
+  );
 }
+const Container = styled.div`
+  position: relative;
+  background-color:#FFFFFF;
+  display: flex;
+  flex-direction: column;
+  /* align-items: center; */
+  padding 0 30px;
+  height: 100vh;
+  
+  `
+  const ContainerHeader = styled.div`
+    padding 10px 30px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color:#FFFFFF;
+    width: 100%;
+    display: flex;
+    border-bottom: 1px solid rgba(0,0,0,.1);
+    div:last-child{
+      background-color: #FFFFFF;
+    }
+  `
+  const BackIcon = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #654C41;
+    border-radius: 4px;
+
+  `
+  const Banner = styled.img`
+    position: absolute;
+    right: 30px;
+    top: -65px;
+    width:353px;
+  
+  `
+  const ContainerProducts = styled.div`
+    margin-top: 220px;
+    display: flex;
+    flex-direction: column;
+    
+
+  `
+  
