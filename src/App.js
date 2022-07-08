5e7d275f56f994fc7c88fca989b3fe47696ccdb1
Flávia Bulad <flavia.bulad@gmail.com>
2022-07-08 15:33:33 -0300

    feat: add basket context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 import WelcomePage from "./components/pages/Welcome/WelcomePage";
 import SignInPage from "./components/pages/SignIn/SignInPage";
@@ -10,18 +11,24 @@ import CheckoutPage from "./components/pages/Checkout/CheckoutPage";
 import "./assets/css/reset.css";
 import "./assets/css/index.css";
 
+import BasketContext from "./components/contexts/BasketContext";
+
 function App() {
+  const [basket, setBasket] = useState(null);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/sign-in/" element={<SignInPage />} />
-        <Route path="/sign-up/" element={<SignUpPage />} />
-        <Route path="/main/" element={<MainPage />} />
-        <Route path="/basket/" element={<BasketPage />} />
-        <Route path="/checkout/" element={<CheckoutPage />} />
-      </Routes>
-    </BrowserRouter>
+    <BasketContext.Provider value={{ basket, setBasket }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/sign-in/" element={<SignInPage />} />
+          <Route path="/sign-up/" element={<SignUpPage />} />
+          <Route path="/main/" element={<MainPage />} />
+          <Route path="/basket/" element={<BasketPage />} />
+          <Route path="/checkout/" element={<CheckoutPage />} />
+        </Routes>
+      </BrowserRouter>
+    </BasketContext.Provider>
   );
 }
 
