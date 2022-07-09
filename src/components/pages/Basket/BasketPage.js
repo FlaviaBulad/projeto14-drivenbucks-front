b9e3850d1f7aae14b50776d7fa5c2e9c42a09ab5
Flávia Bulad <flavia.bulad@gmail.com>
2022-07-09 17:14:29 -0300

    test: render products and layout

--- src/components/pages/Basket/BasketPage.js
+++ src/components/pages/Basket/BasketPage.js
@@ -6,57 +6,108 @@ import { Link, useNavigate } from "react-router-dom";
 
 import { FiArrowLeft } from "react-icons/fi";
 
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import { ThreeDots } from "react-loader-spinner";
-
 import Product from "./Product";
-import { ProductsContext } from "../../contexts/ProductsContext";
+// import { ProductsContext } from "../../contexts/ProductsContext";
 
 export default function BasketPage() {
-  const [products, setProducts] = useContext(ProductsContext);
-  const token = localStorage.getItem("token");
+  // const [products, setProducts] = useContext(ProductsContext);
+  // const token = localStorage.getItem("token");
 
-  useEffect(() => {
-    async function getBasketData() {
-      try {
-        const response = await axios.get("http://localhost:5000/basket", {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        console.log(response);
-        setProducts(response.data);
-      } catch (error) {
-        alert("Erro ao pegar os produtos do carrinho!", error);
-      }
-    }
+  // useEffect(() => {
+  //   async function getBasketData() {
+  //     try {
+  //       const response = await axios.get("http://localhost:5000/basket", {
+  //         headers: {
+  //           Authorization: `Bearer ${token}`,
+  //         },
+  //       });
+  //       console.log(response);
+  //       setProducts(response.data);
+  //     } catch (error) {
+  //       alert("Erro ao pegar os produtos do carrinho!", error);
+  //     }
+  //   }
 
-    getBasketData();
-  }, []);
+  //   getBasketData();
+  // }, []);
+
+  const products = [
+    {
+      name: "café expresso",
+      price: "9.10",
+    },
+    {
+      name: "bolo de banana",
+      price: "14.00",
+    },
+    {
+      name: "torta de limão",
+      price: "25.50",
+    },
+  ];
 
   function buildBasket() {
     if (products.length > 0) {
       return products.map((product, index) => {
-        const { name, value } = product;
-        return <Product key={index} name={name} value={value} />;
+        const { name, price } = product;
+        return <Product key={index} name={name} price={price} />;
       });
     } else {
       return <p>Seu carrinho está vazio!</p>;
     }
   }
 
-  return (
-    <Container>
-      <StyledHeader>
-        <BackIcon>
-          <Link to="/">
-            <FiArrowLeft color={"#545454"} size={30}></FiArrowLeft>
+  const basketSession = buildBasket();
+
+  function buildBalance() {
+    let initialValue = 0;
+
+    if (products.length > 0) {
+      const sum = products
+        .map((product) => Number(product.price))
+        .reduce((prev, curr) => parseFloat(prev + curr, 0));
+      return sum.toFixed(2);
+    } else {
+      return initialValue;
+    }
+  }
+
+  const balanceSection = buildBalance();
+
+  function buildBasketPage() {
+    return (
+      <>
+        <StyledHeader>
+          <BackIcon>
+            <Link to="/main">
+              <FiArrowLeft color={"#545454"} size={30}></FiArrowLeft>
+            </Link>
+          </BackIcon>
+          <h1>Carrinho</h1>
+        </StyledHeader>
+
+        <Titles>
+          <span>Produtos</span> <span>Valor</span>
+        </Titles>
+
+        <BasketContent>{basketSession}</BasketContent>
+
+        <StyledFooter>
+          <div className="total">
+            <span>Total </span>
+            <span>R${balanceSection}</span>
+          </div>
+          <Link to="/checkout">
+            <button>Pagar</button>
           </Link>
-        </BackIcon>
-        <h1>Carrinho</h1>
-      </StyledHeader>
-    </Container>
-  );
+        </StyledFooter>
+      </>
+    );
+  }
+
+  const renderBasketPage = buildBasketPage();
+
+  return <Container>{renderBasketPage}</Container>;
 }
 
 const Container = styled.div`
@@ -64,7 +115,16 @@ const Container = styled.div`
   height: 100%;
   display: flex;
   flex-direction: column;
-  align-items: center;
+`;
+
+const Titles = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+  span {
+    font: 700 12px "Open Sans", sans-serif;
+    color: #000000;
+  }
 `;
 
 const StyledHeader = styled.div`
@@ -77,14 +137,47 @@ const StyledHeader = styled.div`
   justify-content: space-evenly;
 
   align-items: center;
-  border: 2px solid #efefef;
-
+  border-bottom: 2px solid #efefef;
   h1 {
-    font: 700 36px "Quicksand", sans-serif;
+    font: 700 36px "Open Sans", sans-serif;
     color: #654c41;
   }
 `;
 
 const BackIcon = styled.div`
   margin-top: 10px;
+`;
+
+const BasketContent = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const StyledFooter = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 340px;
+  height: 100px;
+  width: 100%;
+  border-top: 2px solid #efefef;
+  .total {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 20px;
+  }
+  span {
+    font: 700 20px "Open Sans", sans-serif;
+    color: #000000;
+  }
+  button {
+    margin-left: 10%;
+    background-color: #654c41;
+    height: 40px;
+    width: 300px;
+    color: #ffffff;
+    border: none;
+    border-radius: 50px;
+    font: 700 20px "Quicksand", sans-serif;
+    cursor: pointer;
+  }
 `;
--- src/components/pages/Basket/Product.js
+++ src/components/pages/Basket/Product.js
@@ -1,11 +1,26 @@
+import styled from "styled-components";
+
 export default function Product(props) {
-  const { name, value } = props;
+  const { name, price } = props;
   return (
-    <div>
-      <div>
-        <p>{name}</p>
-      </div>
-      <p>{value}</p>
-    </div>
+    <List>
+      <span className="name">{name}</span>
+      <span className="price">R${price}</span>
+    </List>
   );
 }
+
+const List = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 2px 20px;
+  .price {
+    font: 700 14px "Open Sans", sans-serif;
+    color: #000000;
+  }
+  .name {
+    font: 400 12px "Open Sans", sans-serif;
+    color: #000000;
+  }
+`;
